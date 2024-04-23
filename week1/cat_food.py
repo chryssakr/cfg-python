2c473ba124e32e7b6e108edc3eebc6c08bd26430
Chryssa Karatza <chryssakaratza@gmail.com>
2024-04-23 20:16:45 +0100

    add session's exercises

---
+++ week1/cat_food.py
@@ -0,0 +1,6 @@
+cats = 10
+cans_per_day = 3
+days = 1
+print(f'For {days} day, {cats} cats require {cats * cans_per_day * days} cans of food.')
+days = 7
+print(f'For {days} days, {cats} cats require {cats * cans_per_day * days} cans of food!')
--- week1/hello.py
+++ week1/hello.py
@@ -1,1 +1,19 @@
 print("Hello World!")
+
+# String and operators
+
+print("cat" + "videos")
+print("cat" * 3)
+# print("cat" + 3) error, can't concatenate string with integers
+print("cat".upper())
+print("cat".lower())
+print("the lord of the rings".title())
+
+# Variables
+username = 'chryssa123'
+age = 27
+print(username, age)
+
+oranges = 12
+cost_per_orange = 0.5
+print(f'{oranges} oranges cost {cost_per_orange * oranges}')
