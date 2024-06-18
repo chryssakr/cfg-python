28df78d5284a5b7798f58eca87b2f6c1410e849d
Chryssa Karatza <chryssakaratza@gmail.com>
2024-06-18 12:54:19 +0100

    add final team project

--- week6/team10_project.py
+++ week6/team10_project.py
@@ -1,63 +1,107 @@
-"""
-before running the program install these libraries if you don't have them
-run this on the terminal:
-pip install pandas
-pip install scipy
-pip install kaggle
-pip install matplotlib
-pip install seaborn
-"""
-import os
-import pandas as pd
-# ignore type because some libraries do not have library stubs
-from scipy.stats import pearsonr # type: ignore
-import matplotlib.pyplot as plt # type: ignore
-import scipy # type: ignore
-import seaborn as sns # type: ignore
-from kaggle.api.kaggle_api_extended import KaggleApi # type: ignore
-import numpy as np
+# CFG Project
 
-# Chrysoula
-def find_mean_speechiness_danceability(df: pd.DataFrame) -> pd.DataFrame:
+import pandas as pd
+from scipy.stats import pearsonr # type: ignore
+import matplotlib.pyplot as plt
+import seaborn as sns # type: ignore
+import numpy as np
+import os.path
+# from kaggle.api.kaggle_api_extended import KaggleApi # type: ignore
+
+def create_dict(df):
+    return df.set_index(['year', 'album']).apply(dict, axis=1)
+
+def get_unique_album(df):
+    """Gets all the unique TS album"""
+    df = df.sort_values(by="release_date") # ensures that album is sorted by release date
+    return df["album"].unique()
+
+# TODO it's not used
+def album_data_popularity(df):
+
+    # Per album get the avg popularity and put in dict
+    for album in album_rows:
+        album_rows = df.loc[
+            df["album"] == album
+        ] # same here as above
+        
+        album_popularity = {}        
+        album_mean_popularity = album_rows.popularity.mean()
+        album_mean_popularity = album_rows.speechiness.mean()
+        album_mean_popularity = album_rows.dancebility.mean()
+        album_mean_popularity = album_rows.temp.mean()
+        album_popularity[album] = album_mean_popularity
+
+    return album_popularity
+
+def find_mean(df: pd.DataFrame) -> pd.DataFrame:
     """
-    Calculate the mean speechiness and danceability per album.
-    I'm using groupby() pandas function to calculate the mean speechiness
-    and danceability per album
+    Calculate the mean attributes per album.
     I first group by "album" name and then use the agg function to specify
-    the "mean" aggregation function for the "speechiness" and "danceability" columns
-    
+    the "mean" aggregation function for the columns I need
+
     Args:
-        df (pd.DataFrame): DataFrame containing the data with columns "album", "speechiness", and "danceability".
+        df (pd.DataFrame): DataFrame containing the data.
 
     Returns:
-        pd.DataFrame: DataFrame with columns "album", "mean_speechiness", and "mean_danceability" representing the mean values per album.
+        pd.DataFrame: DataFrame with columns representing the mean values per album.
     """
-    mean_values = df.groupby("album").agg({
+    mean_values = df.groupby(["album"]).agg({
+        "popularity": "mean",
         "speechiness": "mean",
-        "danceability": "mean"
-    }).reset_index() # this is creating an index column  
+        "danceability": "mean",
+        "energy": "mean",
+        "tempo": "mean"
+    }).reset_index(drop = True)
+
     return mean_values
 
-# Chrysoula
-def calc_pearsonr(mean_values: pd.DataFrame) -> tuple[float, float]:
+def corr_matrix(dfcorr):
+    """Computes a correlation matrix of all numerical variables in the dataset and
+        displays a correlation matrix heatmap"""
+    print("CORRELATION MATRIX")
+    corr_all = dfcorr.corr(
+        method = "pearson",
+        numeric_only = True).round(4) # rounds all the values up to 4 decimal points
+
+    print(corr_all)
+
+    # Save correlation matrix to file
+    with open("results.txt", "a") as file:
+        file.write("\nCORRELATION MATRIX\n")
+        file.write(corr_all.to_string())
+        
+    # Correlation Heatmap
+    mask = np.triu(np.ones_like(corr_all, dtype=bool))
+    sns.heatmap(corr_all,
+                annot = True,
+                vmax = 1,
+                vmin = -1,
+                center = 0,
+                cmap = 'vlag',
+                mask = mask)
+    plt.show()
+
+def calc_pearsonr(song_attribute: str, df2: pd.DataFrame) -> tuple[float, float]:
     """
-    Calculate the Pearson correlation coefficient and p-value between mean speechiness and danceability.
+    Calculate the Pearson correlation coefficient and p-value. Correlating x aspect of a song to its popularity.
 
     Args:
-        mean_values (pd.DataFrame): DataFrame with columns "album", "mean_speechiness", and "mean_danceability".
+        x (str): A song attribute to correlate with popularity
+        df2 (pd.DataFrame): DataFrame with columns "album", "year", "popularity", "speechiness", "danceability", "energy" and "tempo".
 
     Returns:
         tuple[float, float]: Pearson correlation coefficient and p-value.
     """
-    correlation, p_value = pearsonr(mean_values["speechiness"], mean_values["danceability"])
+    correlation, p_value = pearsonr(df2[song_attribute], df2["popularity"])
     p_value = round(p_value, 4)
+
     return correlation, p_value
 
-# Chrysoula
 def interpret_correlation(correlation: float, p_value: float) -> dict:
     """
     Interpret the correlation and p-value results.
-    
+
     Args:
         correlation (float): Pearson correlation coefficient.
         p_value (float): p-value associated with the correlation coefficient.
@@ -78,265 +122,81 @@ def interpret_correlation(correlation: float, p_value: float) -> dict:
         results["evidence"] = {"type": "weak", "message": "A p_value more than than 0.05 indicates that there may not be a significant correlation present in the population, and the observed correlation could have plausibly occurred by chance."}
     return results
 
-# Rose
-def album_data_tempo_(df):
-    # Get all albums in dataset
-    albums = df.album.unique()
-    # get together the unique data of the albums from the data set
-    album_tempos = {} # make a dictionary for the mean tempos
+def plot_data(x_axis, y_axis: float, x, y):
+  plt.scatter(x_axis,y_axis)
+  plt.xlabel(x)
+  plt.ylabel(y)
+  plt.title(f"{y} and {x}")
+  return plt.show()
 
-    # Per album get the avg tempo and put in dict
-    for album in albums:
-        album_rows = df.loc[
-            df["album"] == album # locate the album in the data, use only that albums data
-        ]
-
-        album_mean_tempo = album_rows.tempo.mean() # rows that have tempo
-        album_tempos[album] = album_mean_tempo # get the mean tempo of the albums
-
-    return album_tempos
-
-# Rose
-def album_data_popularity_(df):
-    # Get all albums in dataset
-    albums = df.album.unique()
-    album_popularity = {}
-
-    # Per album get the avg popularity and put in dict
-    for album in albums:
-        album_rows = df.loc[
-            df["album"] == album
-        ] # same here as above
-
-        album_mean_popularity = album_rows.popularity.mean()
-        album_popularity[album] = album_mean_popularity
-
-    return album_popularity
-
-# Camille
-
-"""
-note: df["<var>"] gives you a series while df[["<var>"]] returns a dataframe
-"""
-"""
-Column names for reference:
-Index(['name', 'album', 'release_date', 'track_number', 'id', 'uri',
-       'acousticness', 'danceability', 'energy', 'instrumentalness',
-       'liveness', 'loudness', 'speechiness', 'tempo', 'valence', 'popularity',
-       'duration_ms', 'year'],
-      dtype='object')
-"""
-
-# Camille
-def check_file():
-    """Checks if the file exists in the project root file"""
-    data_file = "CFGProject/taylor_swift_spotify.csv"
-    if os.path.exists(data_file):
-        print("Found dataset archive, loading file...")
-        load_file()
-        return
-    else:
-        print("Dataset not found, using kaggle-api tool for download")
-        download_file()
-
-# Camille
-def download_file():
-    """Downloads file via Kaggle API"""
-    api = KaggleApi()
-    api.authenticate()
-    api.dataset_download_file("jarredpriester/taylor-swift-spotify-dataset",
-                              "taylor_swift_spotify.csv")
-
-# Camille
-def load_file():
-    """Loads the file but initially checks whether the file exists in the project root directory
-       through the check_file() function."""
-    check_file()
-
-    # reads the csv, drops the Unnamed: 0 column, converts release_date's data type, sets index but does not drop it from the dateframe
-    df = (pd.read_csv("taylor_swift_spotify.csv",
-                      usecols=lambda x: x != "Unnamed: 0",          # drops the "Unnamed: 0" column
-                      parse_dates=["release_date"])                 # automatically converts release_date into a datetime data type
-          .sort_values(by="release_date")                           # sorts the dataframe by release_date
-          .set_index("album", drop=False))                    # sets album as index but keeps it in the dataframe
-
-    # creates the year variable
-    df["year"] = df.release_date.dt.year
-
-    # concise data type and non-null count summary of the entire dataframe
-    # print(df.info())
-
-    # gets all the unique TS album in the dataset
-    ts_albums = get_unique_album(df)
-
-    return (df)
-
-# Camille
-def get_unique_album(df):
-    """Gets all the unique TS album"""
-    df = df.sort_values(by="release_date")                          # ensures that album is sorted by release date
-    return df["album"].unique()
-
-# Camille
-def basic_stat(df):
-    """Prints basic statistics
-       note: Can be extended to include other variables"""
-    print("BASIC STATS:")
-    print(df.groupby(df.year)[["energy", "popularity"]].agg(["mean", "min", "max"]))
-    print(df.groupby(df.index)[["energy", "popularity"]].agg(["mean", "min", "max"]))
-
-# Camille
-def corr_matrix(dfcorr):
-    """Computes a correlation matrix of all numerical variables in the dataset and
-        displays a correlation matrix heatmap"""
-    print("CORRELATION MATRIX")
-    corr_all = dfcorr.corr(
-        method = "pearson",
-        numeric_only = True).round(4)                               # rounds all the values up to 4 decimal points
-
-    print(corr_all)
-
-    #Option 1 for heatmap
-    sns.heatmap(corr_all, annot = True, cmap = "coolwarm")
-    plt.show()
-
-    #Option 2 for heatmap
-    mask = np.triu(np.ones_like(corr_all, dtype=bool))
-    sns.heatmap(corr_all,
-                annot = True,
-                vmax = 1,
-                vmin = -1,
-                center = 0,
-                cmap = 'vlag',
-                mask = mask)
-
-    plt.show()
-
-# Camille
-def energy_popularity(df):
-    """Computes the correlation between energy and popularity
-
-    Alternative method for getting correlation using pearson's / simpler version
-    r, p = scipy.stats.pearsonr(df["energy"], df["popularity"])
-    print(r)
-    print(round(p, 4))
-
-    """
-
-    #x = energy, y = popularity
-    slope, intercept, r, p, stderr = scipy.stats.linregress(df["energy"], df["popularity"])
-    print(f"Slope: {slope} \n"
-          f"Intercept {intercept} \n"
-          f"r: {r}\n"
-          f"p-value: {round(p,4)}\n"
-          f"Standard Error: {stderr}")
-
-    #plot_line_corr(df["energy"], df["popularity"], intercept, slope, "energy","population")
-    corr_interpret(r,p,"energy","popularity")
-
-    df["pop_avg_per_album"] = df.groupby(df.index)["popularity"].transform("mean")
-    df["energy_avg_per_album"] = df.groupby(df.index)["energy"].transform("mean")
-    df["pop_avg_per_year"] = df.groupby(["year"])["popularity"].transform("mean")
-    df["energy_avg_per_year"] = df.groupby(["year"])["energy"].transform("mean")
-    print(df[["popularity", "energy"]].agg(["mean", "min", "max"])) #returns a dataframe of stat
-
-    print(f"")
-
-    #print(min(df["pop_avg_per_album"]))
-
-    return (df)
-
-# Camille
-def corr_interpret(r:float,p:float,x:str,y:str):
-    """ Interprets the correlation
-        note: Can be used for the interpretation of other variables"""
-    if r > 0:
-        print(f"There is a positive correlation between {x} and {y}")
-    elif r == 0:
-        print("There is no correlation between the two")
-    elif r < 0:
-        print(f"There is a negative correlation between {x} and {y}")
-
-    if p <= 0.05:
-        print(f"The p-value is p <= 0.05.")
-    elif p == 0:
-        print(f"The p-value = 0 hence no linear relationship exists between {x} and {y}.")
-    elif p >= 0.05:
-        print(f"The p-value is p >= 0.05.")
-
-# Camille
-def plot_line_corr(x_axis: float, y_axis: float, intercept: float, slope: float, x:str,y:str):
-    fig, ax = plt.subplots()
-    ax.plot(x_axis, y_axis, linewidth=0, marker='s', label='Data points')
-    ax.plot(x_axis, intercept + slope * x_axis, label="line")
-    ax.set_xlabel("x")
-    ax.set_ylabel("y")
-    ax.legend(facecolor='white')
-    plt.show()
-
-# Camille
-def main_data_analysis():
+def main():
     """ Main function """
-    # sets the display function of the print function
+    # Sets the display function of the print function
     pd.set_option('display.max_rows', 500)
     pd.set_option('display.max_columns', 20)
     pd.set_option('display.width', 600)
 
-    # download file
-    df = load_file()
-
-    # calls various functions for data analysis and viz
-    basic_stat(df)
-    dfcorrmatrix = df.drop(["release_date", "track_number", "id", "uri", "duration_ms", "year"],
-                           axis=1)
-    corr_matrix(dfcorrmatrix)
-    energy_popularity(df)
-
-def main():
-    """
-    Perform analysis on Taylor Swift's Spotify data.
-
-    Reads the data, calculates mean speechiness and danceability per album, 
-    calculates the Pearson correlation coefficient and p-value,
-    and interprets the correlation results.
-    """
-    # check if the file exists
+    # Download file
     file_path = "data/taylor_swift_spotify.csv"
     if not os.path.exists(file_path):
         print("Error: Data file not found.")
         return
     with open(file_path, "r") as f:
-        df = pd.read_csv(f)
-        
-    # Chryssa's main
-    mean_values = find_mean_speechiness_danceability(df)
-    correlation, p_value = calc_pearsonr(mean_values)
-    results = interpret_correlation(correlation, p_value)
-    print(
-        "\n----------Chrysoula's Analysis----------\n"
-        "I correlated the mean speechiness and danceability per album of Taylor Swift.\n"
-        f"The mean speechiness and danceability per album: \n{mean_values}\n"
-        f"There is a {results["relationship"]["type"]} relationship. {results["relationship"]["message"]}\n"
-        f"The evidence of this correlation is {results["evidence"]["type"]} with a p_value of {p_value}. {results["evidence"]["message"]}\n"
-    )
+        df = pd.read_csv(file_path,
+                      usecols=lambda x: x != "Unnamed: 0",
+                      parse_dates=["release_date"]).sort_values(by="release_date").reset_index(drop = True)
+    df["year"] = df.release_date.dt.year
 
-    # Rose's main
-    album_data = album_data_popularity_(df)
-    album_data = album_data_tempo_(df)
-    print("\n----------Rose's Analysis----------\n")
-    print(album_data)
-    
-    Y = list(album_data_popularity_(df).values())
-    X = list(album_data_tempo_(df).values())
-    
-    new_data = pd.DataFrame([X,Y])
+    # Creates a dictionary of the dataset with year and album as the index (or point of reference)
+    album_dict = create_dict(df)
+    print(album_dict)
 
-    plt.scatter(X,Y)
-    plt.show()
-    
-    # Camille's main
-    "\n----------Camille's Analysis----------\n"
-    main_data_analysis()
-    
+    df2 = df[["album", "year", "popularity", "speechiness", "danceability", "energy", "tempo"]]
+    df2 = df2.set_index(["year", "album"])
+
+    # Gets all the unique TS album in the dataset and lists them all.
+    albums = get_unique_album(df)
+
+    # save the results to a file
+    with open("results.txt", "w") as file:
+        file.write(f"This dataset has {len(albums)} albums. They are: \n")
+        print(f"This dataset has {len(albums)} albums. They are: ")
+        count = 1
+        for album in albums:
+            file.write(f"{count}. {album}\n")
+            print(f"{count}. {album}")
+            count = count + 1
+
+    # Calls various functions for data analysis and viz
+    mean_values = find_mean(df)
+    print(f"The mean values per album\n{mean_values}")
+    with open("results.txt", "a") as file:
+        file.write(f"\nMean values:\n{mean_values}\n")
+
+    # Creates correlation matrix and displays the correlation heatmap
+    dfcorrmatrix = df.drop(["release_date", "track_number", "id", "uri", "duration_ms", "year"],
+                           axis=1)
+    corr_matrix(dfcorrmatrix)
+
+    # Looks at individual correlations between song attributes and popularity
+    print("\n-------Some examples on how to interpret our data:-------")
+    with open("results.txt", "a") as file:
+        file.write("\n-------Some examples on how to interpret our data:-------\n")
+        for song_attribute in df2.drop(["popularity"], axis=1):
+            correlation, p_value = calc_pearsonr(song_attribute, df2)
+            results = interpret_correlation(correlation, p_value)
+            file.write(
+                f"\nCorrelating {song_attribute} & popularity.\n"
+                f"There is a {results['relationship']['type']} relationship. {results['relationship']['message']}\n"
+                f"The evidence of this correlation is {results['evidence']['type']} with a p_value of {p_value}.\n"
+                f"{results['evidence']['message']}\n"
+            )
+            print(
+                f"\nCorrelating {song_attribute} & popularity.\n"
+                f"There is a {results["relationship"]["type"]} relationship. {results["relationship"]["message"]}\n"
+                f"The evidence of this correlation is {results["evidence"]["type"]} with a p_value of {p_value}.\n"
+                f"{results["evidence"]["message"]}\n"
+                )
+
 if __name__ == "__main__":
     main()
